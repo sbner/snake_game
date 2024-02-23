5c2af1e8f67da07cb78be437ffe98881b7585251
Abner <sbner2@gmail.com>
2024-02-22 22:57:30 -0300

    new feature, now the high score gets saved

--- scoreboard.py
+++ scoreboard.py
@@ -5,11 +5,26 @@ TEXT_COLOR = "white"
 TEXT_ALIGN = "center"
 FONT = ('Courier', 16, 'bold')
 SMALLER_FONT = ('Courier', 12, 'bold')
+HIGH_SCORE_FILE = "high_score.txt"
+
+
+def save_high_score(score):
+    with open(HIGH_SCORE_FILE, mode="w") as file:
+        file.write(str(score))
+
+
+def get_high_score():
+    try:
+        with open(HIGH_SCORE_FILE) as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 0
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.high_score = get_high_score()
         self.score = 0
         self.hideturtle()
         self.penup()
@@ -18,14 +33,22 @@ class Scoreboard(Turtle):
         self.print_score()
 
     def increase_score(self, amount=1):
-        self.clear()
         self.score += amount
         self.print_score()
 
     def print_score(self):
-        self.write(f"Score: {self.score}", align=TEXT_ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=TEXT_ALIGN, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            save_high_score(self.high_score)
+        self.score = 0
+        self.print_score()
 
     def game_over(self):
+        self.reset()
         self.home()
         self.write("Game Over.", align=TEXT_ALIGN, font=FONT)
         self.teleport(0,-30)
